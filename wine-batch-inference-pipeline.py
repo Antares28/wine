10fd073f4ce971420cc108cda4319f9c885f6fda
Jian Wei <108125787+Antares28@users.noreply.github.com>
2023-11-19 23:16:07 +0100

    Add files via upload

---
+++ wine-batch-inference-pipeline.py
@@ -0,0 +1,85 @@
+import os
+#import modal
+    
+LOCAL=True
+
+if LOCAL == False:
+   stub = modal.Stub()
+   hopsworks_image = modal.Image.debian_slim().pip_install(["hopsworks","joblib","seaborn","sklearn==1.1.1","dataframe-image"])
+   @stub.function(image=hopsworks_image, schedule=modal.Period(days=1), secret=modal.Secret.from_name("HOPSWORKS_API_KEY"))
+   def f():
+       g()
+
+def g():
+    import pandas as pd
+    import hopsworks
+    import joblib
+    import datetime
+    from PIL import Image
+    from datetime import datetime
+    import dataframe_image as dfi
+    from sklearn.metrics import confusion_matrix
+    from matplotlib import pyplot
+    import seaborn as sns
+    import requests
+
+    project = hopsworks.login()
+    fs = project.get_feature_store()
+    
+    mr = project.get_model_registry()
+    model = mr.get_model("wine_model", version=1)
+    model_dir = model.download()
+    model = joblib.load(model_dir + "/wine_model.pkl")
+    
+    feature_view = fs.get_feature_view(name="wine", version=1)
+    batch_data = feature_view.get_batch_data()
+    
+    y_pred = model.predict(batch_data)
+    #print(y_pred)
+    offset = 1
+    wine = y_pred[y_pred.size-offset]
+    print("Wine quality predicted: " + wine)
+   
+    wine_fg = fs.get_feature_group(name="wine", version=1)
+    df = wine_fg.read() 
+    #print(df)
+    label = df.iloc[-offset]["quality"]
+    print("Wine actual: " + label)
+    
+    monitor_fg = fs.get_or_create_feature_group(name="wine_predictions",
+                                                version=1,
+                                                primary_key=["datetime"],
+                                                description="Wine quality Prediction/Outcome Monitoring"
+                                                )
+    
+    now = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    data = {
+        'prediction': [wine],
+        'label': [label],
+        'datetime': [now],
+       }
+    monitor_df = pd.DataFrame(data)
+    monitor_fg.insert(monitor_df, write_options={"wait_for_job" : False})
+    
+    history_df = monitor_fg.read()
+    # Add our prediction to the history, as the history_df won't have it - 
+    # the insertion was done asynchronously, so it will take ~1 min to land on App
+    history_df = pd.concat([history_df, monitor_df])
+
+
+    df_recent = history_df.tail(4)
+    dfi.export(df_recent, './df_recent.png', table_conversion = 'matplotlib')
+    dataset_api = project.get_dataset_api()    
+    dataset_api.upload("./df_recent.png", "Resources/images", overwrite=True)
+    
+    predictions = history_df[['prediction']]
+    labels = history_df[['label']]
+
+
+if __name__ == "__main__":
+    if LOCAL == True :
+        g()
+    else:
+        with stub.run():
+            f()
+
